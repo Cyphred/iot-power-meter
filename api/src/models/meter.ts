cab2fb532e5ff890d12d6ef77216d3906facce6b
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-11-02 09:46:35 +0800

    Adds power meter model

---
+++ api/src/models/meter.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document, Schema, Types, Model } from "mongoose";
+
+export interface PowerMeter {
+  secret: string;
+  consumer: Types.ObjectId;
+  lastSeen: Date;
+  active: boolean;
+}
+
+export interface PowerMeterDocument extends PowerMeter, Document {}
+
+const PowerMeterSchema: Schema<PowerMeterDocument> =
+  new Schema<PowerMeterDocument>({
+    secret: {
+      type: String,
+      required: true,
+    },
+    consumer: {
+      type: Schema.Types.ObjectId,
+      ref: "Consumer",
+      required: true,
+    },
+    lastSeen: {
+      type: Date,
+      required: true,
+    },
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  });
+
+const PowerMeterModel: Model<PowerMeterDocument> =
+  mongoose.model<PowerMeterDocument>("PowerMeter", PowerMeterSchema);
+
+export default PowerMeterModel;
