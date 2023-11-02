a033be685565afd2b3ddebc22ea3fb443059fee7
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-11-02 10:07:02 +0800

    Updates error codes

--- api/src/errors/errorCodes.ts
+++ api/src/errors/errorCodes.ts
@@ -1,8 +1,20 @@
 export class ErrorCode {
-  static readonly GENERIC = new ErrorCode("PT9999", "Something went wrong");
+  static readonly GENERIC = new ErrorCode("IOTPC9999", "Something went wrong");
   static readonly UNAUTHORIZED = new ErrorCode(
     "IOTPC0001",
     "User is not permitted to perform action"
+  );
+  static readonly METER_NOT_FOUND = new ErrorCode(
+    "IOTPC0002",
+    "Power meter does not exist"
+  );
+  static readonly INVALID_METER_SECRET = new ErrorCode(
+    "IOTPC0003",
+    "Power meter secret is invalid"
+  );
+  static readonly JWT_SECRET_NOT_DEFINED = new ErrorCode(
+    "IOTPC0004",
+    "JWT secret is not defined in local .env"
   );
 
   //to add, just increment the error code
