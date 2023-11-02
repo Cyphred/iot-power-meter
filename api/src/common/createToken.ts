8e5b7dc231e46a49e986dabf729f0eaf7bfb20b0
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-11-02 10:09:06 +0800

    Changes default expiry behavior for tokens

--- api/src/common/createToken.ts
+++ api/src/common/createToken.ts
@@ -4,15 +4,13 @@ import { ErrorCode } from "../errors/errorCodes.js";
 import jwt from "jsonwebtoken";
 dotenv.config();
 
-export default function createToken(
-  payload: object,
-  expiry: string | undefined
-): string {
+export default function createToken(payload: object, expiry?: string): string {
   const secret = process.env.JWT_SECRET;
 
   // Reject if JWT secret not defined in .env
   if (!secret) throw new ApiError(ErrorCode.JWT_SECRET_NOT_DEFINED);
 
+  // Default to 1 day if expiry not defined
   if (!expiry) expiry = "1d";
 
   return jwt.sign(payload, secret, { expiresIn: expiry });
