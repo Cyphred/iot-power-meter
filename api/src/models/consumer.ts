efdd32a87cc08a7d7f8c24445221c2943ea95238
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-11-26 10:16:57 +0800

    Adds signup static method to consumer model

--- api/src/models/consumer.ts
+++ api/src/models/consumer.ts
@@ -1,4 +1,7 @@
 import mongoose, { Document, Schema, Types, Model } from "mongoose";
+import ApiError from "../errors/apiError.js";
+import { ErrorCode } from "../errors/errorCodes.js";
+import bcryptjs from "bcryptjs";
 
 export interface Consumer {
   email: string;
@@ -13,8 +16,24 @@ export interface Consumer {
 
 export interface ConsumerDocument extends Consumer, Document {}
 
-const ConsumerSchema: Schema<ConsumerDocument> = new Schema<ConsumerDocument>({
-  email: { type: String, required: true },
+export interface IConsumerModel extends Model<ConsumerDocument> {
+  signup(args: {
+    email: string;
+    password: string;
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+    streetAddress: string;
+    barangay: string;
+    city: string;
+  }): Promise<ConsumerDocument>;
+}
+
+const ConsumerSchema: Schema<ConsumerDocument, IConsumerModel> = new Schema<
+  ConsumerDocument,
+  IConsumerModel
+>({
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   firstName: { type: String, required: true },
   middleName: { type: String },
@@ -24,7 +43,64 @@ const ConsumerSchema: Schema<ConsumerDocument> = new Schema<ConsumerDocument>({
   city: { type: String, required: true },
 });
 
-export const ConsumerModel: Model<ConsumerDocument> =
-  mongoose.model<ConsumerDocument>("Consumer", ConsumerSchema);
+ConsumerSchema.statics.signup = async function (args: {
+  email: string;
+  password: string;
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  streetAddress: string;
+  barangay: string;
+  city: string;
+}) {
+  const {
+    email,
+    password,
+    firstName,
+    middleName,
+    lastName,
+    streetAddress,
+    barangay,
+    city,
+  } = args;
+
+  // Define model
+  const ConsumerModel = this as Model<ConsumerDocument>;
+
+  // Check if the email is already in use
+  const existing = await ConsumerModel.count({ email });
+
+  // Reject if the email is already in use
+  if (existing) throw new ApiError(ErrorCode.EMAIL_ALREADY_IN_USE);
+
+  // Fetch the number of salt rounds as definde in env
+  const saltRounds = parseInt(process.env.SALT_ROUNDS);
+
+  // Reject if salt rounds definition is invalid
+  if (isNaN(saltRounds)) throw new ApiError(ErrorCode.MISSING_ENV_VALUE);
+
+  // Hash and salt password
+  const salt = await bcryptjs.genSalt(saltRounds);
+  const hash = await bcryptjs.hash(password, salt);
+
+  // Create the consumer account
+  const consumer = await ConsumerModel.create({
+    email,
+    password: hash,
+    firstName,
+    middleName,
+    lastName,
+    streetAddress,
+    barangay,
+    city,
+  });
+
+  return consumer;
+};
+
+export const ConsumerModel: IConsumerModel = mongoose.model<
+  ConsumerDocument,
+  IConsumerModel
+>("Consumer", ConsumerSchema);
 
 export default ConsumerModel;
