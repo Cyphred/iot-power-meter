138310fef913730f4d7c3b8398981deb05fb8a49
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-11-26 11:26:10 +0800

    Adds user types to express request

--- api/src/types/express.d.ts
+++ api/src/types/express.d.ts
@@ -1,5 +1,7 @@
 import { Document, Types } from "mongoose";
 import { PowerMeterDocument } from "../models/meter.ts";
+import { ConsumerDocument } from "../models/consumer.ts";
+import { EmployeeDocument } from "../models/employee.ts";
 
 // For the inclusion of user data in Request objects,
 // add the necessary fields under the Request interface definition
@@ -7,13 +9,9 @@ import { PowerMeterDocument } from "../models/meter.ts";
 declare global {
   namespace Express {
     interface Request {
-      meter?: Document<unknown, {}, PowerMeterDocument> &
-        Omit<
-          PowerMeterDocument & {
-            _id: Types.ObjectId;
-          },
-          never
-        >;
+      meter?: Document<PowerMeterDocument>;
+      consumer?: Document<ConsumerDocument>;
+      employee?: Document<EmployeeDocument>;
     }
   }
 }
