b9c28bacdf33d7101557d0beb7cd98b829e2cf63
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-11-28 18:02:14 +0800

    Re-structures routes to use new middleware setup

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -6,13 +6,23 @@ import {
 } from "./controllers/PowerMeterController.js";
 import requireMeter from "./middleware/requireMeter.js";
 import { login } from "./controllers/loginController.js";
+import { getBill, getPartialBilling } from "./controllers/billingController.js";
+import { requireAuth } from "./middleware/requireAuth.js";
 
 const router = Router();
 
+// Routes that do not require authentication
 router.post("/login", login);
-
 router.post("/meter/get-token", generatePowerMeterToken);
+
+// Routes that can only be accessed by power meters
 router.post("/meter/reports", requireMeter, createPowerMeterReport);
+
+// Routes beyond this point require authentication
+router.use(requireAuth);
+
+router.get("/billing", getBill);
+router.get("/billing/partial", getPartialBilling);
 
 // Do not put routes or other middleware beyond this global handler
 router.use(globalErrorHandler);
