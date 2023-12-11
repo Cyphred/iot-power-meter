8e57095b7edea89505123c53bd013022f586738b
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-11 12:04:39 +0800

    Adds base path to app

--- consumer-web/src/App.tsx
+++ consumer-web/src/App.tsx
@@ -1,15 +1,23 @@
 import { App as AntApp } from "antd";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import AppConfigProvider from "./components/AppConfigProvider";
+import Layout from "./pages/Layout";
 
 function App() {
   return (
     <AntApp>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <AppConfigProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Layout />}>
+              <Route path="dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AppConfigProvider>
     </AntApp>
   );
 }
