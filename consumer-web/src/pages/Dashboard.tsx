f76f28e234b455f3d06f2cbaddfcb92f095f01d1
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-13 06:43:22 +0800

    Implements dynamic display when there is no meter

--- consumer-web/src/pages/Dashboard.tsx
+++ consumer-web/src/pages/Dashboard.tsx
@@ -1,13 +1,17 @@
 import { useEffect } from "react";
 import { useAppSelector } from "../redux/hooks";
 import { useNavigate } from "react-router-dom";
-import { Descriptions } from "antd";
+import { Card, Descriptions, Flex, Typography } from "antd";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
 const Dashboard = () => {
-  const { token, user, stats } = useAppSelector((state) => {
+  const { token, user, stats, meter } = useAppSelector((state) => {
     return {
       token: state.auth.token,
       user: state.auth.user,
+      meter: state.meter.meter,
       stats: state.stats,
     };
   });
@@ -17,8 +21,38 @@ const Dashboard = () => {
     if (!token || !user) navigate("/login");
   }, [token, user]);
 
+  if (!user) return null;
+
+  if (!meter) {
+    return (
+      <Flex
+        vertical
+        style={{ width: "100%", height: "100%" }}
+        justify="center"
+        align="center"
+      >
+        <Card style={{ maxWidth: 400 }}>
+          <Typography.Title level={4} style={{ margin: 0 }}>
+            No Registered Meter
+          </Typography.Title>
+          <Typography.Text>
+            It seems like you currently do not have a meter assigned to you.
+            Contact our customer service hotlines if you think this is an error.
+          </Typography.Text>
+        </Card>
+      </Flex>
+    );
+  }
+
   return (
     <Descriptions title="Stats" layout="vertical" style={{ padding: 16 }}>
+      <Descriptions.Item label="Meter Last Seen">
+        {dayjs(meter.lastSeen).format("hh:mm A MMM DD, YYYY")}
+        <Typography.Text type="secondary">
+          ({dayjs(meter.lastSeen).fromNow()})
+        </Typography.Text>
+      </Descriptions.Item>
+
       <Descriptions.Item label="Average Load">
         {stats.averageLoad.toFixed(4)} KWH
       </Descriptions.Item>
