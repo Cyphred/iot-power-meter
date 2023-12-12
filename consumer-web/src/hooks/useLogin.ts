134d093644b8ea50813b563718c476e25ce776d0
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-13 06:43:10 +0800

    Updates meter parsing in login hook

--- consumer-web/src/hooks/useLogin.ts
+++ consumer-web/src/hooks/useLogin.ts
@@ -2,6 +2,8 @@ import useRequest from "./useRequest";
 import { useAppDispatch } from "../redux/hooks";
 import { setToken, setUser } from "../redux/auth";
 import IConsumer from "../types/Consumer";
+import { setMeter } from "../redux/meter";
+import IMeter from "../types/Meter";
 
 export default function useLogin() {
   const { post, isLoading, apiError, serverError } = useRequest();
@@ -22,11 +24,19 @@ export default function useLogin() {
       return;
     }
 
-    const body = response.body as { token: string; consumer: IConsumer };
+    const body = response.body as {
+      token: string;
+      consumer: IConsumer;
+      meter?: IMeter;
+    };
 
     localStorage.setItem("userToken", body.token);
     dispatch(setToken(body.token));
     dispatch(setUser(body.consumer));
+
+    if (body.meter) {
+      dispatch(setMeter(body.meter));
+    }
   }
 
   return { isLoading, apiError, serverError, login };
