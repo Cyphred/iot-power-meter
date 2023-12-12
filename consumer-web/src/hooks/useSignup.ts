e0e444fe09ce3c8890e1cf349b9043fe6682feb2
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-13 05:28:58 +0800

    Creates signup hook

---
+++ consumer-web/src/hooks/useSignup.ts
@@ -0,0 +1,34 @@
+import useRequest from "./useRequest";
+
+export default async () => {
+  const { isLoading, apiError, serverError, post } = useRequest();
+
+  const signup = async (consumerData: {
+    email: string;
+    password: string;
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+    streetAddress: string;
+    barangay: string;
+    city: string;
+  }) => {
+    const response = await post(
+      `${import.meta.env.VITE_API_URI}/signup/consumer`,
+      consumerData
+    );
+
+    if (!response) {
+      console.error(apiError);
+      console.error(serverError);
+      return false;
+    } else if (response.errorCode) {
+      console.error(response);
+      return false;
+    }
+
+    return true;
+  };
+
+  return { isLoading, apiError, serverError, signup };
+};
