7c37199dee6d5391b8be8dd9cb6ed6b5299dae55
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-13 17:10:06 +0800

    Finalizes dashboard
    - Adds stat re-fetching button
    - Adds data age indicators
    - Adds breakdown table

--- consumer-web/src/pages/Dashboard.tsx
+++ consumer-web/src/pages/Dashboard.tsx
@@ -1,32 +1,48 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppSelector } from "../redux/hooks";
 import { useNavigate } from "react-router-dom";
-import { Card, Descriptions, Flex, Typography } from "antd";
+import { Button, Card, Descriptions, Flex, Table, Typography } from "antd";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import useReports from "../hooks/useReports";
+import BreakdownTable from "../components/BreakdownTable";
 dayjs.extend(relativeTime);
 
 const Dashboard = () => {
-  const { token, user, consumption, meter, rate } = useAppSelector((state) => {
-    return {
-      token: state.auth.token,
-      user: state.auth.user,
-      meter: state.meter.meter,
-      consumption: state.stats.consumption?.consumption,
-      rate: state.stats.consumption?.ratePerKwh,
-    };
-  });
+  const { token, user, consumption, meter, rate, rateBreakdown } =
+    useAppSelector((state) => {
+      return {
+        token: state.auth.token,
+        user: state.auth.user,
+        meter: state.meter.meter,
+        consumption: state.stats.consumption?.consumption,
+        rate: state.stats.consumption?.ratePerKwh,
+        rateBreakdown: state.stats.consumption?.rateBreakdown,
+      };
+    });
   const navigate = useNavigate();
-  const { getConsumptionReport } = useReports();
+  const { getConsumptionReport, isLoading } = useReports();
+  const [lastPulledStats, setLastPulledStats]: [
+    Date | undefined,
+    React.Dispatch<React.SetStateAction<Date | undefined>>
+  ] = useState();
+
+  const handleGetConsumptionReport = async () => {
+    const result = await getConsumptionReport();
+    if (result) setLastPulledStats(new Date());
+  };
 
   useEffect(() => {
-    getConsumptionReport();
+    handleGetConsumptionReport();
   }, []);
 
   useEffect(() => {
     if (!token || !user) navigate("/login");
   }, [token, user]);
+
+  const handleReloadStats = () => {
+    handleGetConsumptionReport();
+  };
 
   if (!user) return null;
 
@@ -52,55 +68,96 @@ const Dashboard = () => {
   }
 
   return (
-    <Descriptions title="Stats" layout="vertical" style={{ padding: 16 }}>
-      <Descriptions.Item label="Meter Last Seen">
-        {dayjs(meter.lastSeen).format("hh:mm A MMM DD, YYYY")}
-        <Typography.Text type="secondary">
-          ({dayjs(meter.lastSeen).fromNow()})
-        </Typography.Text>
-      </Descriptions.Item>
+    <>
+      <Descriptions
+        title={
+          <Flex
+            style={{ width: "100%" }}
+            gap={8}
+            align="center"
+            justify="space-between"
+          >
+            <>Stats</>
 
-      <Descriptions.Item label="Average Load">
-        {consumption ? (
-          <>{consumption.averageDaily.toFixed(4)} KWH</>
-        ) : (
-          <>No data</>
-        )}
-      </Descriptions.Item>
+            <Flex align="center" gap={8}>
+              <Typography.Text
+                type="secondary"
+                style={{ fontWeight: "normal" }}
+              >
+                Last reloaded{" "}
+                {!lastPulledStats ? (
+                  "never"
+                ) : (
+                  <>
+                    {dayjs(lastPulledStats).format("hh:mm:ss A MMM DD, YYYY")}
+                    <br />({dayjs(lastPulledStats).fromNow()})
+                  </>
+                )}
+              </Typography.Text>
+              <Button
+                type="default"
+                onClick={handleReloadStats}
+                disabled={isLoading}
+              >
+                Reload
+              </Button>
+            </Flex>
+          </Flex>
+        }
+        layout="vertical"
+        style={{ padding: 16 }}
+      >
+        <Descriptions.Item label="Meter Last Seen">
+          {dayjs(meter.lastSeen).format("hh:mm A MMM DD, YYYY")}
+          <Typography.Text type="secondary">
+            ({dayjs(meter.lastSeen).fromNow()})
+          </Typography.Text>
+        </Descriptions.Item>
 
-      <Descriptions.Item label="Current Load">
-        {consumption && consumption.rightNow ? (
-          dayjs(new Date()).diff(consumption.rightNow.timestamp, "seconds") >=
-          10 ? (
-            <>No data - meter has been offline for too long.</>
+        <Descriptions.Item label="Average Daily Load">
+          {consumption ? (
+            <>{consumption.averageDaily.toFixed(4)} KWH</>
           ) : (
-            <>{consumption.rightNow.value.toFixed(4)} W</>
-          )
-        ) : (
-          <>No data</>
-        )}
-      </Descriptions.Item>
+            <>No data</>
+          )}
+        </Descriptions.Item>
 
-      <Descriptions.Item label="This month's consumption">
-        {consumption ? (
-          <> {consumption.sinceCutoff.toFixed(4)} KWH </>
-        ) : (
-          <>No data</>
-        )}
-      </Descriptions.Item>
+        <Descriptions.Item label="Current Load">
+          {consumption && consumption.rightNow ? (
+            dayjs(new Date()).diff(consumption.rightNow.timestamp, "seconds") >=
+            10 ? (
+              <>No data - meter has been offline for too long.</>
+            ) : (
+              <>{consumption.rightNow.value.toFixed(4)} W</>
+            )
+          ) : (
+            <>No data</>
+          )}
+        </Descriptions.Item>
 
-      <Descriptions.Item label="Current bill">
-        {rate && consumption ? (
-          <>
-            Php. {(consumption.sinceCutoff * rate).toFixed(2)} @ Php.{" "}
-            {rate.toFixed(2)}
-            /KWH
-          </>
-        ) : (
-          <>No data</>
-        )}
-      </Descriptions.Item>
-    </Descriptions>
+        <Descriptions.Item label="This month's consumption">
+          {consumption ? (
+            <> {consumption.sinceCutoff.toFixed(4)} KWH </>
+          ) : (
+            <>No data</>
+          )}
+        </Descriptions.Item>
+
+        <Descriptions.Item label="Current bill">
+          {rate && consumption ? (
+            <>
+              Php. {(consumption.sinceCutoff * rate).toFixed(2)} @ Php.{" "}
+              {rate.toFixed(2)}
+              /KWH
+            </>
+          ) : (
+            <>No data</>
+          )}
+        </Descriptions.Item>
+      </Descriptions>
+
+      <BreakdownTable />
+    </>
   );
 };
 
