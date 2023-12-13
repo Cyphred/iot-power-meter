7d9c737d38580356d94d5f19f696e3d8e8a9037e
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-13 17:09:11 +0800

    Cleans up layout

--- consumer-web/src/pages/Layout.tsx
+++ consumer-web/src/pages/Layout.tsx
@@ -1,11 +1,9 @@
-import { Layout as AntLayout, Button, Menu, Typography, theme } from "antd";
+import { Layout as AntLayout, Button, Typography, theme } from "antd";
 import { Outlet } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
 import { useAppSelector } from "../redux/hooks";
 
 const { Header, Content, Footer } = AntLayout;
-
-const links = [{ key: 0, label: "" }];
 
 const Layout = () => {
   const { logout } = useLogout();
@@ -25,20 +23,6 @@ const Layout = () => {
     <AntLayout className="layout" style={{ width: "100vw", height: "100vh" }}>
       <Header style={{ display: "flex", alignItems: "center" }}>
         <div className="demo-logo" />
-        {/*
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={["2"]}
-          items={new Array(15).fill(null).map((_, index) => {
-            const key = index + 1;
-            return {
-              key,
-              label: `nav ${key}`,
-            };
-          })}
-        />
-        */}
         <Typography.Text style={{ color: "white" }}>
           Logged in as {user?.firstName} {user?.lastName}
         </Typography.Text>
@@ -47,7 +31,7 @@ const Layout = () => {
       <Content style={{ padding: "0 50px", overflow: "scroll" }}>
         <div
           className="site-layout-content"
-          style={{ background: colorBgContainer, height: "100%" }}
+          style={{ background: colorBgContainer, paddingBottom: 16 }}
         >
           <Outlet />
         </div>
