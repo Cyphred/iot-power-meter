b46dca6bf35cfb7d7de13dc920b5c51520a6efd5
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-14 06:45:24 +0800

    Adds menu to dashboard

--- employee-web/src/pages/Layout.tsx
+++ employee-web/src/pages/Layout.tsx
@@ -1,11 +1,42 @@
-import { Layout as AntLayout, Button, Flex, Typography, theme } from "antd";
-import { Outlet } from "react-router-dom";
+import {
+  Layout as AntLayout,
+  Button,
+  Flex,
+  Menu,
+  MenuProps,
+  Typography,
+  theme,
+} from "antd";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
 import { useAppSelector } from "../redux/hooks";
+import { useEffect, useState } from "react";
 
-const { Header, Content, Footer } = AntLayout;
+const { Header, Content } = AntLayout;
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: "group"
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+}
+
+const items: MenuItem[] = [getItem("Subscribers", "1"), getItem("Rates", "2")];
 
 const Layout = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
   const { logout } = useLogout();
   const { user } = useAppSelector((state) => {
     return { user: state.auth.user };
@@ -19,13 +50,23 @@ const Layout = () => {
     token: { colorBgContainer },
   } = theme.useToken();
 
+  const handleMenuSetPage = (key: string) => {
+    switch (key) {
+      case "1":
+        navigate("/subscribers");
+        break;
+      case "2":
+        navigate("/rates");
+        break;
+    }
+  };
+
   return (
     <AntLayout className="layout" style={{ width: "100vw", height: "100vh" }}>
       <Header
         style={{
           display: "flex",
           alignItems: "center",
-          backgroundColor: "#f04134",
         }}
       >
         <Flex
@@ -37,6 +78,15 @@ const Layout = () => {
           <Typography.Title level={5} style={{ margin: 0, color: "white" }}>
             Employee Dashboard
           </Typography.Title>
+
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            items={items}
+            style={{ flex: 1, minWidth: 0 }}
+            onSelect={(info) => handleMenuSetPage(info.key)}
+          />
+
           <Flex gap={8} align="center">
             <Typography.Text style={{ color: "white" }}>
               Logged in as {user?.firstName} {user?.lastName}
@@ -53,9 +103,6 @@ const Layout = () => {
           <Outlet />
         </div>
       </Content>
-      <Footer style={{ textAlign: "center" }}>
-        Ant Design ©2023 Created by Ant UED
-      </Footer>
     </AntLayout>
   );
 };
