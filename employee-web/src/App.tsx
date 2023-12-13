9a6ff6a0f949ade1ee9bd351607217156d08ec24
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-14 05:57:14 +0800

    Adds signup to router

--- employee-web/src/App.tsx
+++ employee-web/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppConfigProvider from "./components/AppConfigProvider";
 import Login from "./pages/Login";
 import Layout from "./pages/Layout";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
             <Route path="/" element={<Layout />}></Route>
           </Routes>
         </BrowserRouter>
