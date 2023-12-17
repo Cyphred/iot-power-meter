41550adaa8eb8a98fde3b8a4bea20d0f79dc79ea
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-17 11:30:54 +0800

    Improves auth

--- employee-web/src/hooks/useAuth.ts
+++ employee-web/src/hooks/useAuth.ts
@@ -1,16 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import { setToken, setUser } from "../redux/auth";
 import { useAppDispatch } from "../redux/hooks";
 import Employee from "../types/Employee";
 
 export default () => {
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const getAuthFromLocalStorage = () => {
     try {
       const usertoken = localStorage.getItem("userToken");
       const employee = localStorage.getItem("user");
 
-      if (!employee || !usertoken) return;
-
+      if (!employee) throw new Error();
       const parsedEmployee = JSON.parse(employee) as Employee;
 
       dispatch(setToken(usertoken));
@@ -19,6 +20,7 @@ export default () => {
       return { usertoken, parsedEmployee };
     } catch (err) {
       console.error(err);
+      navigate("/login");
     }
   };
 
