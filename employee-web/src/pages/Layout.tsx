7493e68f07c60075530de1714f27ff58a34bb54e
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-17 11:13:43 +0800

    Gets rid of menu in favor of buttons

--- employee-web/src/pages/Layout.tsx
+++ employee-web/src/pages/Layout.tsx
@@ -1,42 +1,11 @@
-import {
-  Layout as AntLayout,
-  Button,
-  Flex,
-  Menu,
-  MenuProps,
-  Typography,
-  theme,
-} from "antd";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Layout as AntLayout, Button, Flex, Typography, theme } from "antd";
+import { Link, Outlet } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
 import { useAppSelector } from "../redux/hooks";
-import { useEffect, useState } from "react";
 
 const { Header, Content } = AntLayout;
 
-type MenuItem = Required<MenuProps>["items"][number];
-
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[],
-  type?: "group"
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type,
-  } as MenuItem;
-}
-
-const items: MenuItem[] = [getItem("Subscribers", "1"), getItem("Rates", "2")];
-
 const Layout = () => {
-  const { pathname } = useLocation();
-  const navigate = useNavigate();
   const { logout } = useLogout();
   const { user } = useAppSelector((state) => {
     return { user: state.auth.user };
@@ -50,23 +19,16 @@ const Layout = () => {
     token: { colorBgContainer },
   } = theme.useToken();
 
-  const handleMenuSetPage = (key: string) => {
-    switch (key) {
-      case "1":
-        navigate("/subscribers");
-        break;
-      case "2":
-        navigate("/rates");
-        break;
-    }
-  };
-
   return (
     <AntLayout className="layout" style={{ width: "100vw", height: "100vh" }}>
       <Header
         style={{
           display: "flex",
           alignItems: "center",
+          backgroundColor: "white",
+          borderStyle: "solid",
+          borderWidth: "0 0 1px 0",
+          borderColor: "rgba(0,0,0,0.15)",
         }}
       >
         <Flex
@@ -75,20 +37,21 @@ const Layout = () => {
           justify="space-between"
           style={{ width: "100%" }}
         >
-          <Typography.Title level={5} style={{ margin: 0, color: "white" }}>
+          <Typography.Title level={5} style={{ margin: 0 }}>
             Employee Dashboard
           </Typography.Title>
 
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            items={items}
-            style={{ flex: 1, minWidth: 0 }}
-            onSelect={(info) => handleMenuSetPage(info.key)}
-          />
+          <Flex justify="center" align="center" gap={8}>
+            <Link to="/subscribers">
+              <Button type="primary">Subscribers</Button>
+            </Link>
+            <Link to="/rates">
+              <Button type="primary">Rates</Button>
+            </Link>
+          </Flex>
 
           <Flex gap={8} align="center">
-            <Typography.Text style={{ color: "white" }}>
+            <Typography.Text>
               Logged in as {user?.firstName} {user?.lastName}
             </Typography.Text>
             <Button onClick={handleLogout}>Log out</Button>
