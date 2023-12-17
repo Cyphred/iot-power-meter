94be81297b035dfd72179f8d8d16f5e001efaa71
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-17 12:27:46 +0800

    Adds report getter

---
+++ employee-web/src/hooks/useReports.ts
@@ -0,0 +1,27 @@
+import IConsumptionReport from "../types/ConsumptionReport";
+import useRequest from "./useRequest";
+
+export default () => {
+  const { isLoading, apiError, serverError, authorizedGet } = useRequest();
+
+  const getConsumptionReport = async (consumerId: string) => {
+    const response = await authorizedGet(
+      `${import.meta.env.VITE_API_URI}/reports/consumption/${consumerId}`
+    );
+
+    if (!response) {
+      console.error(apiError);
+      console.error(serverError);
+      return;
+    } else if (response.errorCode) {
+      console.error(response);
+      return;
+    }
+
+    const report = response.body as IConsumptionReport;
+
+    return report;
+  };
+
+  return { isLoading, apiError, serverError, getConsumptionReport };
+};
