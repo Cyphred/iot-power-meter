7463258b8a194d3c873ebfe8082f5c55690503d4
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-20 22:44:08 +0800

    Creates hook for creating bills

--- employee-web/src/hooks/useBilling.ts
+++ employee-web/src/hooks/useBilling.ts
@@ -1,8 +1,11 @@
 import IBilling from "../types/Billing";
+import ICutoff from "../types/Cutoff";
+import IRate from "../types/Rates";
 import useRequest from "./useRequest";
 
 export default () => {
-  const { isLoading, apiError, serverError, authorizedGet } = useRequest();
+  const { isLoading, apiError, serverError, authorizedGet, authorizedPost } =
+    useRequest();
 
   const getPendingBill = async (consumerId: string) => {
     const response = await authorizedGet(
@@ -26,10 +29,53 @@ export default () => {
     return bill;
   };
 
+  const generateBills = async () => {
+    const response = await authorizedPost(
+      `${import.meta.env.VITE_API_URI}/billing/generate-for-all`,
+      {
+        breakdown: [
+          {
+            description: "System generation charge",
+            amount: 9.99,
+          },
+          {
+            description: "Misc charge 1",
+            amount: 1.23,
+          },
+          {
+            description: "Misc charge 2",
+            amount: 0.003,
+          },
+        ],
+      }
+    );
+
+    if (!response) {
+      console.error(apiError);
+      console.error(serverError);
+      return;
+    }
+
+    if (apiError || serverError) {
+      if (apiError) console.error(apiError);
+      if (serverError) console.error(serverError);
+      return;
+    }
+
+    const { billsCreated, rate, cutoff } = response.body as {
+      billsCreated: IBilling;
+      rate: IRate;
+      cutoff: ICutoff;
+    };
+
+    return { billsCreated, rate, cutoff };
+  };
+
   return {
     isLoading,
     apiError,
     serverError,
     getPendingBill,
+    generateBills,
   };
 };
